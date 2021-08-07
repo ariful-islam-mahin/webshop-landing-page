0437db318cbe20b7ea9c39df123b328417833f59
ariful-islam-mahin <mahinyayaa@gmail.com>
2021-08-07 20:23:48 +0600

    inline css added

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -3,8 +3,8 @@ import './MainHeader.css'
 
 const MainHeader = () => {
     return (
-        <div id="main-header" className="text-center mb-5">
-            <div className="main-header-text text-white">
+        <div id="main-header" className="text-center mb-5" style={{background: 'url("https://i.ibb.co/pzJbZPr/image-32image.png") no-repeat center', backgroundSize: "cover", height: "411px", position: "relative"}}>
+            <div className="main-header-text text-white" style={{maxWidth: "620px !important", padding: "65px", position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", background: "#011b7b59"}}>
                 <h1>The gift of happiness</h1>
                 <p>Show your gratitude with one of our gift sets. large and small, specially designed to spread joy to someone you love.</p>
                 <button className="btn mx-2 gradient-btn rounded-pill text-white px-4">SHOP NOW</button>
