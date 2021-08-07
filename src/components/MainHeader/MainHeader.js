6cde38974478975b803bff3e840bfbe02ac49f55
ariful-islam-mahin <mahinyayaa@gmail.com>
2021-08-07 19:23:00 +0600

    fix a bug

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -3,7 +3,7 @@ import './MainHeader.css'
 
 const MainHeader = () => {
     return (
-        <div className="main-header text-center mb-5">
+        <div id="main-header" className="text-center mb-5">
             <div className="main-header-text text-white">
                 <h1>The gift of happiness</h1>
                 <p>Show your gratitude with one of our gift sets. large and small, specially designed to spread joy to someone you love.</p>
